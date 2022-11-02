7fd0d31f99b5bd344e60f41033ecc8214862d5c1
Michael-Eyieta <114599216+Michael-Eyieta@users.noreply.github.com>
2022-11-01 19:57:31 -0500

    Nov 1 Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+
+const App = () => {
+    return <div>
+        <Router>
+
+        </Router exact path= >
+    </div>;
+};
+
+export default App;
